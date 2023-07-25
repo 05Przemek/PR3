b8f98e20712172ac934696c279e89b1c8dc1ac42
05Przemek <przemochmielowiec@gmail.com>
2023-07-25 12:56:15 +0200

    drugi

--- string 1/Program.cs
+++ string 1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace string_1
+{
+    class Program
+{
+    static void Main()
+    {
+        string imie;
+
+        imie = "Ania";
+        int Nieprzeczytane = 7;
+        int Powiadomienia = 9;
+
+        string powitanie = Generujpowitanie(imie, Nieprzeczytane, Powiadomienia);
+
+        Console.WriteLine($"Witaj {imie}! Masz {Nieprzeczytane} nieprzeczytanych wiadomości oraz {Powiadomienia} powiadomień.");
+    }
+
+    static string Generujpowitanie(string imie, int Nieprzeczytane, int Powiadomienia)
+    {
+        string output = $"Witaj {imie}! Masz {Nieprzeczytane} nieprzeczytanych wiadomości oraz {Powiadomienia} powiadomień.";
+        return output;
+    }
+}
+}
